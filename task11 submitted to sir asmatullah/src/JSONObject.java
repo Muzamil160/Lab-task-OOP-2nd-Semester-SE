3ccec7175911b6df5eccf7f2b203b14bddc1c91a
Muzamil160 <114946527+Muzamil160@users.noreply.github.com>
2022-10-03 23:04:08 +0500

    Add files via upload

---
+++ task11 submitted to sir asmatullah/src/JSONObject.java
@@ -0,0 +1,12 @@
+public class JSONObject {
+    public void put(String name, String name1) {
+    }
+
+    public int toJSONString() {
+        return 0;
+    }
+
+    public Object get(String gender) {
+        return gender;
+    }
+}
---
+++ task11 submitted to sir asmatullah/src/JSONParser.java
@@ -0,0 +1,8 @@
+import java.io.FileReader;
+
+public class
+JSONParser {
+    public Object parse(FileReader fileReader) {
+    return  fileReader;
+    }
+}
